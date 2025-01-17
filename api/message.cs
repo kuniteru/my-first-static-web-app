3b8979fae20d02d6f0455f943f3e50815bc29105
Kuniteru Asami <kuniteru@asami.asia>
2025-01-17 12:12:31 +0900

    feat: enhance response message to include request headers

--- api/message.cs
+++ api/message.cs
@@ -25,9 +25,16 @@ namespace Company.Function
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            // req.headersのすべてのキーと値を1つの文字列変数に代入する
+            string headers = "";
+            foreach (var header in req.Headers)
+            {
+                headers += header.Key + ": " + header.Value + "\n";
+            }
+
+            string responseMessage = string.IsNullOrEmpty(headers)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"{headers}";
 
             return new OkObjectResult(responseMessage);
         }
